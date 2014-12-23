4d7d0ff2b0e5d706f0c77f46e919171d3f71e2e8
Guo Yu <i@guoyu.me>
2014-12-23 21:39:38 +0800

    Add api rules

--- tests/search.js
+++ tests/search.js
@@ -4,6 +4,8 @@ var utils = require('../lib/utils');
 describe('#Starwood', function() {
   describe('#Search', function() {
     it('Should return correct search results', function(done) {
+      this.timeout(6000);
+
       starwood.search({
         country: 'CN',
         province: 'CNHP',
